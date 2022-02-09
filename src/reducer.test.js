13ca3e4c4b4558a1312e032f9d0b450216de6322
iMMoCodes <matti-makela@hotmail.com>
2022-02-09 21:50:43 +0200

    Tehtävät 6.1 & 6.2

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -3,15 +3,15 @@ import counterReducer from './reducer'
 
 describe('unicafe reducer', () => {
   const initialState = {
-    good: 0,
-    ok: 0,
-    bad: 0
+    good: 5,
+    ok: 4,
+    bad: 2,
   }
 
   test('should return a proper initial state when called with undefined state', () => {
     const state = {}
     const action = {
-      type: 'DO_NOTHING'
+      type: 'DO_NOTHING',
     }
 
     const newState = counterReducer(undefined, action)
@@ -20,16 +20,61 @@ describe('unicafe reducer', () => {
 
   test('good is incremented', () => {
     const action = {
-      type: 'GOOD'
+      type: 'GOOD',
     }
     const state = initialState
 
     deepFreeze(state)
     const newState = counterReducer(state, action)
     expect(newState).toEqual({
-      good: 1,
-      ok: 0,
-      bad: 0
+      good: 6,
+      ok: 4,
+      bad: 2,
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK',
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 5,
+      ok: 5,
+      bad: 2,
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD',
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 5,
+      ok: 4,
+      bad: 3,
+    })
+  })
+
+  test('reset works', () => {
+    const action = {
+      type: 'ZERO',
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 5,
+      ok: 4,
+      bad: 2,
     })
   })
 })
